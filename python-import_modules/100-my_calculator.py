8e53ea7b86227d8ad718f8d6831ece395c359675
otienodiana <d.otieno@alustudent.com>
2022-10-28 10:29:18 -0700

    funtion for importing

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,28 +1,23 @@
-#!/usr/bin/pyhon3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    args = len(argv)
-
-    if args != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-        a = int(argv[1])
-        op = argv[2]
-        b = int(argv[3])
-
-        if op == "+":
-            num = add(a, b)
-        elif op == "-":
-            num = sub(a, b)
-        elif op == "*":
-            num = mul(a, b)
-        elif op == "/":
-            num = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-            print("{:d} {} {:d} = {:d}".format(a, op, b, num))
-
+    import calculator_1 as extra
+    import sys
+argNum = len(sys.argv)  # gets number of arguments
+la = sys.argv  # stores arguments return by sys module
+if argNum != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+else:
+    a = int(la[1])
+    b = int(la[3])
+    if la[2] == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, extra.sub(a, b)))
+    elif la[2] == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, extra.mul(a, b)))
+    elif la[2] == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, extra.div(a, b)))
+    elif la[2] == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, extra.add(a, b)))
+    else:
+        `print("Unknown operator. Available operators: +, -, * and /")
+                       exit(1)
