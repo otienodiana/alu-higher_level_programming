96bc0f871fa425bc370510ad0b13ed76011d186a
otienodiana <d.otieno@alustudent.com>
2022-10-28 10:49:29 -0700

    function for calculation

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import calculator_1 as extra
     import sys
-argNum = len(sys.argv)  # gets number of arguments
-la = sys.argv  # stores arguments return by sys module
-if argNum != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-else:
-    a = int(la[1])
-    b = int(la[3])
-    if la[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, extra.sub(a, b)))
-    elif la[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, extra.mul(a, b)))
-    elif la[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, extra.div(a, b)))
-    elif la[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, extra.add(a, b)))
-    else:
+    i = len(sys.argv) - 1
+    if i != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    sign = sys.argv[2]
+    if sign != '+' and sign != '-' and sign != '*' and sign != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-                       exit(1)
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sign == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sign == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sign == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
