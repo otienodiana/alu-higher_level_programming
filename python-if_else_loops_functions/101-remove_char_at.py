6a19371028e1201f24ff0001685feebd8f73e7ed
otienodiana <d.otieno@alustudent.com>
2022-10-28 09:47:39 -0700

    function for calculation

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return (98 + a ** b)
+def remove_char_at(str, n):
+    new = ''
+    i = 0
+    for c in str:
+        if i != n:
+            new += str[i]
+        i += 1
+    return new
