a596fb10ecdcb773f30fae66f9354452735ab6cc
otienodiana <d.otieno@alustudent.com>
2022-10-28 09:56:28 -0700

    function for adding

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 from add_0 import add
 
-if__name__ == "__main__":
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+a = 1
+b = 2
+if __name__ == '__main__':
+    print("{} + {} = {}".format(a, b, add(a, b)))
