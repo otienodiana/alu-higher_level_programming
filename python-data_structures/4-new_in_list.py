42734d6d8807dbba0661e630da2bdca938d6f74c
otienodiana <d.otieno@alustudent.com>
2022-11-02 12:04:19 -0700

    funtions for new list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list) -1:
         return new_list
     else:
-        my_list[idx] = element
+        new_list[idx] = element
         return new_list
