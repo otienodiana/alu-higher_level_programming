28b1878a64568535a874f466b2737b75bed3ff87
otienodiana <d.otieno@alustudent.com>
2022-11-02 11:31:48 -0700

    funtons listsa

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len( my_list) -1:
-        return my_list[:]
-    new_list = my_list[:]
-    new_list[idx] = element
-    return new _list
+    if idx < 0 or idx >= len(my_list) -1:
+        return my_list
+    my_list[idx] = element
+    return my_list
