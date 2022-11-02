cf92768b1958d6e236f3016bf16aaaa1558af53e
otienodiana <d.otieno@alustudent.com>
2022-11-02 04:52:05 -0700

    functions of return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        print("None")
-    elif idx >= my_list:
-        print("None")
-
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+        
