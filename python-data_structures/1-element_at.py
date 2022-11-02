4e95efce99391e94bf07f92436ddd2e086d6723a
otienodiana <d.otieno@alustudent.com>
2022-11-02 04:46:06 -0700

    functions for displaying

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < my_list:
+    if idx < 0:
         print("None")
-    elif idx > my_list:
+    elif idx >= my_list:
         print("None")
 
