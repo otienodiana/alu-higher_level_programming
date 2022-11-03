6e2ded9dae4948b22db4023fa955d9057820caf0
otienodiana <d.otieno@alustudent.com>
2022-11-03 09:04:34 -0700

    functions forignoring characters

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord("c"):None, ord("C"):None})
-    return new_string
-print(no_c("Best School"))
-print(no_c("Chicago")) 
-print(no_c("C is fun!"))
+    my_string = my_string.translate({ord("c"): None})
+    my_string = my_string.translate({ord("C"): None})
+    return my_string
