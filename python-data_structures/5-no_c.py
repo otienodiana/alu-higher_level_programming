bb13ebd5b0d0368aa997a82b1cc8686af0e8a3b9
otienodiana <d.otieno@alustudent.com>
2022-11-03 05:51:38 -0700

    funtions for ignoring caharacters

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
         if "c" and "C" in my_string:
             new_string = my_string.translate({ord("c"):None, ord("C"):None})
-            print(new_string)
+            return new_string
         else:
-            print("c is is not")
+            return  c is not
