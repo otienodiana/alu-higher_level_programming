565b6564f44ac5248960886ce7607efa83adbc6b
your name <your email>
2022-12-08 09:45:16 -0800

    script for adding intergers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-
-"""a function that adds 2 integers"""
-
+"""Defines an addition function"""
 
 def add_integer(a, b=98):
-    """
-    Adds and returns sum of a and b
-    - Args :
-        a: int or float
-        b: int or float, default 98
-    """
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
+    """Adds two integers"""
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-                                                      return a + b
+    return int(a) + int(b)
