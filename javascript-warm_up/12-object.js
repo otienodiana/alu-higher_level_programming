13461d7cd999f7f17abb4d4fe493ad28de8dcd50
Your Name <you@example.com>
2023-02-02 22:30:53 +0300

    codes for replacing the value

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-// script to replace the value 12 with 89
+// script to replace  12 with 89
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
